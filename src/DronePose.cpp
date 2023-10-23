83810d3e86142ac0abd3a6a38daf07b4ffb24236
Fixit-Davide <dgraziato10@gmail.com>
2023-10-23 16:34:35 +0200

    small changes

--- src/DronePose.cpp
+++ src/DronePose.cpp
@@ -62,44 +62,47 @@ void DronePose::position_cb(
   geometry_msgs::msg::PoseStamped pose_msg;
   visualization_msgs::msg::Marker arrow_velocity_msg;
   rclcpp::Time time = get_clock()->now();
-
-  pose_msg.header.frame_id = base_frame;
-  pose_msg.header.stamp = time;
-  pose_msg.pose.orientation.w = msg_att->q[0];
-  pose_msg.pose.orientation.x = msg_att->q[1];
-  pose_msg.pose.orientation.y = -msg_att->q[2];
-  pose_msg.pose.orientation.z = -msg_att->q[3];
-  pose_msg.pose.position.x = msg_loc_pos->x;
-  pose_msg.pose.position.y = -msg_loc_pos->y;
-  pose_msg.pose.position.z = -msg_loc_pos->z;
-  vehicle_pose_pub_->publish(std::move(pose_msg));
-
-  arrow_velocity_msg.action = 0;
-  arrow_velocity_msg.header.frame_id = base_frame;
-  arrow_velocity_msg.header.stamp = time;
-  arrow_velocity_msg.ns = "arrow";
-  arrow_velocity_msg.type = 0;
-  arrow_velocity_msg.scale.x = 0.1;
-  arrow_velocity_msg.scale.y = 0.2;
-  arrow_velocity_msg.scale.z = 0.1;
-  arrow_velocity_msg.color.r = 0.5;
-  arrow_velocity_msg.color.g = 0.5;
-  arrow_velocity_msg.color.b = 0.0;
-  arrow_velocity_msg.color.a = 1.0;
-  float dt = 0.2;
-  geometry_msgs::msg::Point tail, head;
-  tail.x = msg_loc_pos->x;
-  tail.y = -msg_loc_pos->y;
-  tail.z = -msg_loc_pos->z;
-  head.x = msg_loc_pos->x + dt * msg_loc_pos->vx;
-  head.y = -msg_loc_pos->y + dt * -msg_loc_pos->vy;
-  head.z = -msg_loc_pos->z + dt * -msg_loc_pos->vz;
-  arrow_velocity_msg.points = {tail, head};
-  vehicle_vel_pub_->publish(std::move(arrow_velocity_msg));
-
-  path_msg->header = pose_msg.header;
-  path_msg->poses.push_back(pose_msg);
-  vehicle_path_pub_->publish(*path_msg);
+  if(pub_pose){
+    pose_msg.header.frame_id = base_frame;
+    pose_msg.header.stamp = time;
+    pose_msg.pose.orientation.w = msg_att->q[0];
+    pose_msg.pose.orientation.x = msg_att->q[1];
+    pose_msg.pose.orientation.y = -msg_att->q[2];
+    pose_msg.pose.orientation.z = -msg_att->q[3];
+    pose_msg.pose.position.x = msg_loc_pos->x;
+    pose_msg.pose.position.y = -msg_loc_pos->y;
+    pose_msg.pose.position.z = -msg_loc_pos->z;
+    vehicle_pose_pub_->publish(std::move(pose_msg));
+  }
+  if(pub_vel){
+    arrow_velocity_msg.action = 0;
+    arrow_velocity_msg.header.frame_id = base_frame;
+    arrow_velocity_msg.header.stamp = time;
+    arrow_velocity_msg.ns = "arrow";
+    arrow_velocity_msg.type = 0;
+    arrow_velocity_msg.scale.x = 0.1;
+    arrow_velocity_msg.scale.y = 0.2;
+    arrow_velocity_msg.scale.z = 0.1;
+    arrow_velocity_msg.color.r = 0.5;
+    arrow_velocity_msg.color.g = 0.5;
+    arrow_velocity_msg.color.b = 0.0;
+    arrow_velocity_msg.color.a = 1.0;
+    float dt = 0.2;
+    geometry_msgs::msg::Point tail, head;
+    tail.x = msg_loc_pos->x;
+    tail.y = -msg_loc_pos->y;
+    tail.z = -msg_loc_pos->z;
+    head.x = msg_loc_pos->x + dt * msg_loc_pos->vx;
+    head.y = -msg_loc_pos->y + dt * -msg_loc_pos->vy;
+    head.z = -msg_loc_pos->z + dt * -msg_loc_pos->vz;
+    arrow_velocity_msg.points = {tail, head};
+    vehicle_vel_pub_->publish(std::move(arrow_velocity_msg));
+  }
+  if(pub_path){
+    path_msg->header = pose_msg.header;
+    path_msg->poses.push_back(pose_msg);
+    vehicle_path_pub_->publish(*path_msg);
+  }
 
   // tf broadcaster: base_link of the drone
   geometry_msgs::msg::TransformStamped tf;
